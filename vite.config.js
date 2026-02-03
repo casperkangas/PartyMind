801cce64a481f33144fc1210e23895214b1b9ef5
casperkangas <casperkangas@gmail.com>
2026-02-04 00:29:46 +0200

    Remove homepage entry from package.json and comment out unnecessary base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PartyMind/",
+  // base: "/PartyMind/", // This is unecessary because of Vercel
 });
